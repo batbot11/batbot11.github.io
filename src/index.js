f4d250f8377f4ed9c0bd60dfa97cab29a9fc0a9c
batbot11 <40738848+batbot11@users.noreply.github.com>
2019-01-03 15:05:22 +0530

    redux with middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createLogger} from "redux-logger";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import App from './containers/App';
 import "./index.css"
 import {searchRobots} from "./reducers";
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 
 ReactDOM.render(
 <Provider store={store}>
