6675f5009532abab424c53e492f8bcaa8f7a3958
batbot11 <40738848+batbot11@users.noreply.github.com>
2019-01-04 10:37:27 +0530

    redux async complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunkMiddleware from "redux-thunk";
 import {createLogger} from "redux-logger";
 import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
 import App from './containers/App';
 import "./index.css"
-import {searchRobots} from "./reducers";
+import {searchRobots, requestRobots} from "./reducers";
 import * as serviceWorker from './serviceWorker';
 
 const logger = createLogger();
-
-const store = createStore(searchRobots, applyMiddleware(logger))
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 
 ReactDOM.render(
