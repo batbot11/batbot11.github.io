3281d9d6ea764aa5e4e591cebaa99c72189e8cf2
batbot11 <40738848+batbot11@users.noreply.github.com>
2019-01-04 21:37:17 +0530

    Updates

---
+++ precache-manifest.6b90bf77ce255ceb0390a711b0eafe96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "320e90d68a4dd43b700e",
+    "url": "/robofriends/static/js/main.320e90d6.chunk.js"
+  },
+  {
+    "revision": "b36bf072a85e1c94a910",
+    "url": "/robofriends/static/js/1.b36bf072.chunk.js"
+  },
+  {
+    "revision": "320e90d68a4dd43b700e",
+    "url": "/robofriends/static/css/main.31a3ce56.chunk.css"
+  },
+  {
+    "revision": "b36bf072a85e1c94a910",
+    "url": "/robofriends/static/css/1.208c30bc.chunk.css"
+  },
+  {
+    "revision": "65bff588a37fd574cbed38ae867ceac0",
+    "url": "/robofriends/index.html"
+  }
+];
